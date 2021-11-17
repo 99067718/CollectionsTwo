0f6edb6bf94aa8d2af153befd67f3f3d04169072
Emiel 't Lam <99067718@mydavinci.nl>
2021-11-17 12:48:08 +0100

    sinterklaas komt eraan

--- SinterklaasLootjes_Generator.py
+++ SinterklaasLootjes_Generator.py
@@ -1,1 +1,72 @@
-naam = input("Wat is je naam?: ")
+import math
+import time
+import random
+namenLijst = []
+ongebruikteNamen = []
+getrokkenLootje = {}
+HasGivenAmount = False
+
+while HasGivenAmount == False:
+    try:
+        aantal = int(input("Met hoeveel mensen doet U dit?: "))
+        if aantal <= 2:
+            print("U moet op zijn minst met zijn drieën zijn om lootjes te trekken, vul wat anders in.")
+        else:
+            HasGivenAmount = True
+    except ValueError:
+        print("Probeer het opnieuw")
+def Namen():
+    for i in range(aantal):
+        naam = input("naam "+str(i + 1)+": ")
+        if naam in namenLijst:
+            print("Deze naam staat al in de lijst, weet U zeker dat U deze in de lijst wil toevoegen?")
+            while True:
+                proceed = input("Y/N: ").upper()
+                if proceed == "Y":
+                    namenLijst.append(naam)
+                    break
+                elif proceed == "N":
+                    print("Vul een nieuwe naam in.")
+                    naam = input("naam "+str(i + 1)+": ")
+                    namenLijst.append(naam)
+                    #getrokkenLootje[naam] = ""
+                    break
+                else:
+                    print("Sorry dat begreep ik niet.")
+        else:        
+            namenLijst.append(naam)
+            #getrokkenLootje[naam] = ""
+
+def randomLootje():
+    print("Lootjes zijn aan het verdelen...")
+    numbersBetween = random.randint(1,aantal)
+    for i in range(aantal):
+        nummerdingetje = numbersBetween + -i
+        Getrokken = namenLijst[nummerdingetje]
+        Persoon = namenLijst[i]
+        getrokkenLootje[Persoon] = Getrokken
+
+Namen()
+
+while True:
+    print("Weet U zeker dat deze lijst klopt?")
+    time.sleep(1)
+    print(namenLijst)
+    correct = input("Y/N: ").upper()
+    if correct == "Y":
+        break
+    elif correct == "N":
+        namenLijst = []
+        getrokkenLootje = {}
+        Namen()
+    else:
+        print("Sorry dat begreep ik niet.")
+
+randomLootje()
+print(getrokkenLootje)
+
+
+
+
+
+
